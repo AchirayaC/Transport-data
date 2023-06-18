556abd0f1ec2d880370c012155b6537fb9801236
PChunark <puttipongchunark@gmail.com>
2023-06-18 22:23:08 +0700

    Plot a road length

--- code/th_road.R
+++ code/th_road.R
@@ -1,9 +1,25 @@
-th_road <- 
+library(tidyverse)
+library(readxl)
+
+th_road <-
   read_excel("rawdata/Road length.xlsx",
-             range = "B4:C20")
+             range = "B5:C20",
+             col_names = c("year", "rdlength"))
+
+# Find max and min value 
+maxv <- ceiling(max(th_road$rdlength))
+minv <- floor(min(th_road$rdlength))
 
 ggplot(data = th_road)+
-  geom_line(aes(x = Year, y = "Road length"))+
-  geom_point(aes(x = Year, y = "Road length"))+
+  geom_line(aes(x = year, 
+                y = rdlength),
+            color ="red", # Color the line
+            show.legend = TRUE)+ # Remove legend from the line plot
+  geom_point(aes(x = year, 
+                 y = rdlength),
+             shape = 4, # Change the shape of the point
+             size = 2, # Adjust the size of the point
+             color = "blue")+ # Color the shape
+  scale_x_continuous(breaks = seq(2007,2022,3))+
   labs(x = NULL, 
        y = "Road length (km)")
